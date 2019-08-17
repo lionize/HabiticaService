c817d8f840c3f82cf86ac25de0b515d91ece730d
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-17 19:51:46 +0300

    Update Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebAPI
+namespace TIKSN.Lionize.WebAPI
 {
     public class Program
     {
