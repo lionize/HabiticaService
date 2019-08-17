c534763c2a64a3325b55d38f43aa29ab97e529cc
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-17 19:56:27 +0300

    Update Startup.cs

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,11 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace TIKSN.Lionize.WebAPI
 {
     public class Startup
     {
+        private readonly string AllowSpecificCorsOrigins = "_AllowSpecificCorsOrigins_";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +29,24 @@ namespace TIKSN.Lionize.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("1.0", new OpenApiInfo { Title = "Lionize / Task Management Service", Version = "1.0" });
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllowSpecificCorsOrigins,
+                cpbuilder =>
+                {
+                    var origins = Configuration.GetSection("Cors").GetSection("Origins").Get<string[]>();
+
+                    cpbuilder.AllowAnyMethod();
+                    cpbuilder.AllowAnyHeader();
+                    cpbuilder.WithOrigins(origins);
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +61,15 @@ namespace TIKSN.Lionize.WebAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/1.0/swagger.json", "API 1.0");
+            });
+
+            app.UseCors(AllowSpecificCorsOrigins);
 
             app.UseHttpsRedirection();
             app.UseMvc();
