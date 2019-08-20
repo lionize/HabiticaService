584b444c30f7349c6fa5b24b2fd92a46366eb368
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-20 13:05:08 +0300

    Create UserProfileSettings.cs

---
+++ src/Data/Entities/UserProfileSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TIKSN.Data;
+
+namespace TIKSN.Lionize.Data.Entities
+{
+    public class UserProfileSettings : IEntity<Guid>
+    {
+        public string HabiticaApiTokenProtected { get; set; }
+
+        public string HabiticaUserID { get; set; }
+
+        public Guid ID { get; set; }
+
+        public Guid UserID { get; set; }
+    }
+}
