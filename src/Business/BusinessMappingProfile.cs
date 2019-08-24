a359de80d827b654ed2e29c418a657538c55dfc0
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-24 20:09:02 +0300

    Create BusinessMappingProfile.cs

---
+++ src/Business/BusinessMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.DataProtection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Business
+{
+    public class BusinessMappingProfile : Profile
+    {
+        public BusinessMappingProfile(IDataProtectionProvider provider)
+        {
+            protector = provider.CreateProtector("Contoso.MyClass.v1");
+        }
+    }
+}
