a4cebf69718bd2b1e97305fc54d3eee53641e130
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-24 19:25:55 +0300

    Create UserProfileSettingsModel.cs

---
+++ src/Business/Models/UserProfileSettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TIKSN.Lionize.Business.Models
+{
+    internal class UserProfileSettingsModel
+    {
+        public string HabiticaApiToken { get; set; }
+
+        public string HabiticaUserID { get; set; }
+
+        public Guid ID { get; set; }
+
+        public Guid UserID { get; set; }
+    }
+}
