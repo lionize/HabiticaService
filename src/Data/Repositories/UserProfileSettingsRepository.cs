5a4f23b9ac215ec07111092a0c4aadcdd7282971
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-25 13:59:18 +0300

    Create UserProfileSettingsRepository.cs

---
+++ src/Data/Repositories/UserProfileSettingsRepository.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data.Mongo;
+using TIKSN.Lionize.HabiticaTaskProviderService.Data.Entities;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Repositories
+{
+    public class UserProfileSettingsRepository : MongoRepository<UserProfileSettingsEntity, Guid>
+    {
+        public UserProfileSettingsRepository(IMongoDatabaseProvider mongoDatabaseProvider) : base(mongoDatabaseProvider, "UserProfileSettings")
+        {
+        }
+
+        public async Task<IReadOnlyCollection<UserProfileSettingsEntity>> ListUserSettingsAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            var cursor = await collection.FindAsync(Builders<UserProfileSettingsEntity>.Filter.Eq(item => item.UserID, userId), options: null, cancellationToken);
+            return await cursor.ToListAsync(cancellationToken);
+        }
+    }
+}
