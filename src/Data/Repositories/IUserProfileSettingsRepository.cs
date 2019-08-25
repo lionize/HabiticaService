2b8fd3c704087c8ab1de3bdb3bfc6721119c3add
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-25 14:37:20 +0300

    Create IUserProfileSettingsRepository.cs

---
+++ src/Data/Repositories/IUserProfileSettingsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data.Mongo;
+using TIKSN.Lionize.HabiticaTaskProviderService.Data.Entities;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Repositories
+{
+    public interface IUserProfileSettingsRepository : IMongoRepository<UserProfileSettingsEntity, Guid>
+    {
+        Task<IReadOnlyCollection<UserProfileSettingsEntity>> ListAsync(Guid userId, CancellationToken cancellationToken);
+    }
+}
