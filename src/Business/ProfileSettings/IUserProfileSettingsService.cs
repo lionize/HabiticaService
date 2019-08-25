db4243378cc5ac917aa7e5af913fcee646b3cbf5
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-25 15:25:53 +0300

    Create IUserProfileSettingsService.cs

---
+++ src/Business/ProfileSettings/IUserProfileSettingsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Business.ProfileSettings
+{
+    public interface IUserProfileSettingsService
+    {
+        Task<UserProfileSettingsCredentialModel> GetCredentialAsync(Guid id, CancellationToken cancellationToken);
+        Task<IReadOnlyCollection<UserProfileSettingsRetrievalModel>> ListAsync(Guid userId, CancellationToken cancellationToken);
+        Task UpdateAsync(Guid id, Guid curentUserId, UserProfileSettingsUpdateModel updateModel, CancellationToken cancellationToken);
+    }
+}
