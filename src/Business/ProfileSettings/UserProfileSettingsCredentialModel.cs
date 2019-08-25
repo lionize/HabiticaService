8b4c1c6f361a3f02f81816f77d2f9d437e065183
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-25 15:24:06 +0300

    Create UserProfileSettingsCredentialModel.cs

---
+++ src/Business/ProfileSettings/UserProfileSettingsCredentialModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TIKSN.Lionize.HabiticaTaskProviderService.Business.ProfileSettings
+{
+    public class UserProfileSettingsCredentialModel
+    {
+        public string HabiticaApiToken { get; set; }
+        public string HabiticaUserID { get; set; }
+    }
+}
