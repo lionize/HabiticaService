b783c2aed29ea3de2477576dabde522ac7466396
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-25 15:34:15 +0300

    Update SettingsController.cs

--- src/WebAPI/Controllers/V1/SettingsController.cs
+++ src/WebAPI/Controllers/V1/SettingsController.cs
@@ -1,7 +1,11 @@
-﻿using Lionize.HabiticaTaskProvider.ApiModels.V1;
+﻿using AutoMapper;
+using Lionize.HabiticaTaskProvider.ApiModels.V1;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
+using TIKSN.Lionize.HabiticaTaskProviderService.Business.ProfileSettings;
 
 namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.Controllers.V1
 {
@@ -11,10 +15,24 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.Controllers.V1
     [Authorize]
     public class SettingsController : ControllerBase
     {
-        [HttpGet]
-        public async Task<SettingsGetterResponse> Get()
+        private readonly IMapper _mapper;
+        private readonly IUserProfileSettingsService _userProfileSettingsService;
+
+        public SettingsController(IMapper mapper, IUserProfileSettingsService userProfileSettingsService)
         {
-            return null;
+            _userProfileSettingsService = userProfileSettingsService ?? throw new ArgumentNullException(nameof(userProfileSettingsService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public async Task<SettingsGetterResponse> Get(CancellationToken cancellationToken)
+        {
+            var models = await _userProfileSettingsService.ListAsync(Guid.Parse(User.Identity.Name), cancellationToken);
+
+            return new SettingsGetterResponse
+            {
+                Settings = _mapper.Map<SettingsGetterItem[]>(models)
+            };
         }
 
         [HttpPut("{id}")]
