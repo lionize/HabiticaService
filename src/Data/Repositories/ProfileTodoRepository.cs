c1b2ee69784bb2b36a6245d8e580be51f39303ef
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 18:36:40 +0300

    Create ProfileTodoRepository.cs

---
+++ src/Data/Repositories/ProfileTodoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TIKSN.Data.Mongo;
+using TIKSN.Lionize.HabiticaTaskProviderService.Data.Entities;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Repositories
+{
+    public class ProfileTodoRepository : MongoRepository<ProfileTodoEntity, Guid>
+    {
+        public ProfileTodoRepository(IMongoDatabaseProvider mongoDatabaseProvider) : base(mongoDatabaseProvider, "ProfileTodos")
+        {
+        }
+    }
+}
