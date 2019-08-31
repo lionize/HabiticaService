a9d91854c21a2f906734914ce79ad198e1c4e905
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 22:11:04 +0300

    Create IntegrationAutofacModule.cs

---
+++ Integration/IntegrationAutofacModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Integration
+{
+    public class IntegrationAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EndpointAddressProvider>()
+                .As<IEndpointAddressProvider>()
+                .SingleInstance();
+        }
+    }
+}
