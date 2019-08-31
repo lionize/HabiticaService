c9b95cc65eed98c080a5fef831b5f559dbff947d
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 18:41:34 +0300

    Update ProfileTodoRepository.cs

--- src/Data/Repositories/ProfileTodoRepository.cs
+++ src/Data/Repositories/ProfileTodoRepository.cs
@@ -4,7 +4,7 @@ using TIKSN.Lionize.HabiticaTaskProviderService.Data.Entities;
 
 namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Repositories
 {
-    public class ProfileTodoRepository : MongoRepository<ProfileTodoEntity, Guid>
+    public class ProfileTodoRepository : MongoRepository<ProfileTodoEntity, Guid>, IProfileTodoRepository
     {
         public ProfileTodoRepository(IMongoDatabaseProvider mongoDatabaseProvider) : base(mongoDatabaseProvider, "ProfileTodos")
         {
