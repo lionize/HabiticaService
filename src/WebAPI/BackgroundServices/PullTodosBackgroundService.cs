9d07f1c258ed071c980b0ccc9e601e58e539f281
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 17:41:41 +0300

    Update PullTodosBackgroundService.cs

--- src/WebAPI/BackgroundServices/PullTodosBackgroundService.cs
+++ src/WebAPI/BackgroundServices/PullTodosBackgroundService.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
+using TIKSN.Lionize.HabiticaTaskProviderService.Data.Repositories;
 
 namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.BackgroundServices
 {
     public class PullTodosBackgroundService : BackgroundService
     {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IUserProfileSettingsRepository _userProfileSettingsRepository;
+
+        public PullTodosBackgroundService(IServiceProvider serviceProvider, IUserProfileSettingsRepository userProfileSettingsRepository)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            _userProfileSettingsRepository = userProfileSettingsRepository ?? throw new ArgumentNullException(nameof(userProfileSettingsRepository));
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             await Task.Delay(TimeSpan.FromHours(1)); //TODO: Get From Configuration
