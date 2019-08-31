4dc667a867cd7c26d14519af1e3be7b81c130e1c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 21:55:48 +0300

    Create TaskUpserted.cs

---
+++ Integration/Events/TaskUpserted.cs
@@ -0,0 +1,9 @@
+﻿namespace TIKSN.Lionize.HabiticaTaskProviderService.Integration.Events
+{
+    public interface TaskUpserted
+    {
+        bool Completed { get; }
+
+        string Text { get; }
+    }
+}
