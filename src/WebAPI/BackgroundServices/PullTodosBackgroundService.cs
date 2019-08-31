0deec9c83f0d2853ce037a7ba9f5b5b41f14cb83
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 17:34:48 +0300

    Create PullTodosBackgroundService.cs

---
+++ src/WebAPI/BackgroundServices/PullTodosBackgroundService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.BackgroundServices
+{
+    public class PullTodosBackgroundService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Delay(TimeSpan.FromHours(1)); //TODO: Get From Configuration
+        }
+    }
+}
