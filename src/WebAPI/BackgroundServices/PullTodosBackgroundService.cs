5ea329b365e2e71f5d63f7757d94ca079833cf72
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 18:34:13 +0300

    Update PullTodosBackgroundService.cs

--- src/WebAPI/BackgroundServices/PullTodosBackgroundService.cs
+++ src/WebAPI/BackgroundServices/PullTodosBackgroundService.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 using TIKSN.Habitica.Rest;
 using TIKSN.Lionize.HabiticaTaskProviderService.Business.ProfileSettings;
 using TIKSN.Lionize.HabiticaTaskProviderService.Business.Settings;
+using TIKSN.Lionize.HabiticaTaskProviderService.Data.Entities;
 using TIKSN.Lionize.HabiticaTaskProviderService.Data.Repositories;
 
 namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.BackgroundServices
@@ -13,6 +16,9 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.BackgroundServices
     public class PullTodosBackgroundService : BackgroundService
     {
         private readonly IHabiticaClient _habiticaClient;
+        private readonly ILogger<PullTodosBackgroundService> _logger;
+        private readonly IMapper _mapper;
+        private readonly IProfileTodoRepository _profileTodoRepository;
         private readonly IServiceProvider _serviceProvider;
         private readonly IUserProfileSettingsRepository _userProfileSettingsRepository;
         private readonly IUserProfileSettingsService _userProfileSettingsService;
@@ -20,13 +26,19 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.BackgroundServices
         public PullTodosBackgroundService(
             IServiceProvider serviceProvider,
             IUserProfileSettingsRepository userProfileSettingsRepository,
+            IProfileTodoRepository profileTodoRepository,
             IUserProfileSettingsService userProfileSettingsService,
-            IHabiticaClient habiticaClient)
+            IHabiticaClient habiticaClient,
+            IMapper mapper,
+            ILogger<PullTodosBackgroundService> logger)
         {
             _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
             _userProfileSettingsRepository = userProfileSettingsRepository ?? throw new ArgumentNullException(nameof(userProfileSettingsRepository));
             _userProfileSettingsService = userProfileSettingsService ?? throw new ArgumentNullException(nameof(userProfileSettingsService));
             _habiticaClient = habiticaClient ?? throw new ArgumentNullException(nameof(habiticaClient));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _profileTodoRepository = profileTodoRepository ?? throw new ArgumentNullException(nameof(profileTodoRepository));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -46,7 +58,7 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.BackgroundServices
                         var credentials = await _userProfileSettingsService.GetCredentialAsync(profile.ID, stoppingToken);
                         credentialSettings.Store(credentials.HabiticaUserID, credentials.HabiticaApiToken);
 
-                        await PullUserTodosAsync(stoppingToken);
+                        await PullUserTodosAsync(profile.ID, profile.UserID, stoppingToken);
 
                         await Task.Delay(TimeSpan.FromSeconds(30)); //TODO: Get From Configuration
                     }
@@ -56,9 +68,24 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI.BackgroundServices
             await Task.Delay(TimeSpan.FromHours(1)); //TODO: Get From Configuration
         }
 
-        private async Task PullUserTodosAsync(CancellationToken cancellationToken)
+        private async Task PullUserTodosAsync(Guid profileID, Guid userID, CancellationToken cancellationToken)
         {
             var todos = await _habiticaClient.GetUserToDosAsync(cancellationToken);
+
+            if (todos.Success)
+            {
+                foreach (var todo in todos.Data)
+                {
+                    var entity = new ProfileTodoEntity { ProviderProfileID = profileID, ProviderUserID = userID };
+                    entity = _mapper.Map(todo, entity);
+
+                    await _profileTodoRepository.AddOrUpdateAsync(entity, cancellationToken);
+                }
+            }
+            else
+            {
+                _logger.LogError("Failed to get user todos.");
+            }
         }
     }
 }
