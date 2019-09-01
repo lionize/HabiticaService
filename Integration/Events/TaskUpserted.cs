b1ae012abe351e92ec29a36653677007cb1d1867
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-01 21:25:06 +0300

    Delete TaskUpserted.cs

--- Integration/Events/TaskUpserted.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TIKSN.Lionize.HabiticaTaskProviderService.Integration.Events
-{
-    public interface TaskUpserted
-    {
-        bool Completed { get; }
-
-        string Text { get; }
-    }
-}
