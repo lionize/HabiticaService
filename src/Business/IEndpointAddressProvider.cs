57db1e7cdc406aa8bc038befb5c4174bfe0d366b
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-01 21:25:20 +0300

    Create IEndpointAddressProvider.cs

---
+++ src/Business/IEndpointAddressProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Business
+{
+    public interface IEndpointAddressProvider
+    {
+        Uri GetEndpointAddress(string queueName);
+    }
+}
