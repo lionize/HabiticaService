42e40860e365b9c567e4b15fc1496c94631111a6
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-01 21:21:21 +0300

    Create TaskUpserted.cs

---
+++ src/Business/Messages/Integration/TaskUpserted.cs
@@ -0,0 +1,9 @@
+﻿namespace TIKSN.Lionize.HabiticaTaskProviderService.Business.Messages.Integration
+{
+    public interface TaskUpserted
+    {
+        bool Completed { get; }
+
+        string Text { get; }
+    }
+}
