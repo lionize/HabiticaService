b01269dd24a4ddca65fb706d975f030f966e203d
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-01 21:43:36 +0300

    Create UpsertTodoRequest.cs

---
+++ src/Business/Messages/Domain/UpsertTodoRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using TIKSN.Habitica.Models;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Business.Messages.Domain
+{
+    public class UpsertTodoRequest : IRequest
+    {
+        public UpsertTodoRequest(TaskData data)
+        {
+            Data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        public TaskData Data { get; }
+    }
+}
