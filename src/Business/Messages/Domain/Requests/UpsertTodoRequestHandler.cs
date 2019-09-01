479e3c44142ee3905bd581a7c684d455ce4683ee
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-01 21:48:53 +0300

    Create UpsertTodoRequestHandler.cs

---
+++ src/Business/Messages/Domain/Requests/UpsertTodoRequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Business.Messages.Domain.Requests
+{
+    public class UpsertTodoRequestHandler : IRequestHandler<UpsertTodoRequest>
+    {
+        public async Task<Unit> Handle(UpsertTodoRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
