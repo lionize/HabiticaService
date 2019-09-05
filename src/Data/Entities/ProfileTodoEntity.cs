b999e62234a7fd1895d53696b7af9625c8770318
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-06 00:41:09 +0300

    Update ProfileTodoEntity.cs

--- src/Data/Entities/ProfileTodoEntity.cs
+++ src/Data/Entities/ProfileTodoEntity.cs
@@ -1,7 +1,9 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using TIKSN.Data;
+using TIKSN.Lionize.HabiticaTaskProviderService.Data.Serializers;
 
 namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Entities
 {
@@ -40,9 +42,11 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Entities
 
         public double Priority { get; set; }
 
-        public Guid ProviderProfileID { get; set; }
+        [BsonSerializer(typeof(BigIntegerSerializer))]
+        public BigInteger ProviderProfileID { get; set; }
 
-        public Guid ProviderUniformID { get; set; }
+        [BsonSerializer(typeof(BigIntegerSerializer))]
+        public BigInteger ProviderUniformID { get; set; }
 
         public Guid ProviderUserID { get; set; }
 
