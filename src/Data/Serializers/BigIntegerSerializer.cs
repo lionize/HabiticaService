fdd8802d9fb6748d7573013823f5cd481b5ef364
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-06 00:41:51 +0300

    Create BigIntegerSerializer.cs

---
+++ src/Data/Serializers/BigIntegerSerializer.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System.Numerics;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Serializers
+{
+    public class BigIntegerSerializer : SerializerBase<BigInteger>
+    {
+        public override BigInteger Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            string val = context.Reader.ReadString();
+            return BigInteger.Parse(val);
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, BigInteger value)
+        {
+            context.Writer.WriteString(value.ToString());
+        }
+    }
+}
