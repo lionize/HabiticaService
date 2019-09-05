c62fdd6bda6653232452845bb94f400a3309fbda
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-06 01:01:09 +0300

    Update BusinessAutofacModule.cs

--- src/Business/BusinessAutofacModule.cs
+++ src/Business/BusinessAutofacModule.cs
@@ -6,6 +6,7 @@ using TIKSN.Habitica.Settings;
 using TIKSN.Lionize.HabiticaTaskProviderService.Business.IdentityGenerator;
 using TIKSN.Lionize.HabiticaTaskProviderService.Business.ProfileSettings;
 using TIKSN.Lionize.HabiticaTaskProviderService.Business.Settings;
+using TIKSN.Serialization.Numerics;
 
 namespace TIKSN.Lionize.HabiticaTaskProviderService.Business
 {
@@ -32,6 +33,9 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.Business
 
             builder.RegisterType<UnsignedBigIntegerIdentityGenerator>()
                 .As<IIdentityGenerator<BigInteger>>()
+                .SingleInstance();
+
+            builder.RegisterType<UnsignedBigIntegerBinaryDeserializer>()
                 .SingleInstance();
         }
     }
