7a96fe9e41d68147381b98b31039f1cc844c12ec
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-01 23:33:16 +0300

    Create IHabiticaProfileService.cs

---
+++ src/Business/ProfileSettings/IHabiticaProfileService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Lionize.HabiticaTaskProviderService.Business.ProfileSettings
+{
+    public interface IHabiticaProfileService
+    {
+        Task<HabiticaProfileModel> GetAsync(string userId, string apiKey, CancellationToken cancellationToken);
+    }
+}
