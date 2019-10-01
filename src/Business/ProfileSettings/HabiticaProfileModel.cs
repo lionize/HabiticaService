5ef3e8a820ee59301de67932a5288a9c69835599
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-01 23:26:18 +0300

    Create HabiticaProfileModel.cs

---
+++ src/Business/ProfileSettings/HabiticaProfileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TIKSN.Lionize.HabiticaTaskProviderService.Business.ProfileSettings
+{
+    public class HabiticaProfileModel
+    {
+        public string FullName { get; }
+
+        public string Username { get; }
+    }
+}
