38474dc03a8a8f0e472ba8772da2b4ea76972960
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-05 16:49:46 +0300

    Update Program.cs

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,28 +1,41 @@
-﻿using Microsoft.AspNetCore;
+﻿using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 using System.Threading.Tasks;
+using TIKSN.DependencyInjection;
+using TIKSN.Habitica;
 using TIKSN.Lionize.Messaging.Services;
 
 namespace TIKSN.Lionize.HabiticaTaskProviderService.WebAPI
 {
     public class Program
     {
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        public static IHostBuilder CreateWebHostBuilder(string[] args)
         {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseSerilog((context, configuration) =>
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
                 {
-                    configuration
-                        .MinimumLevel.Debug()
-                        //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                        //.MinimumLevel.Override("System", LogEventLevel.Warning)
-                        //.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                        .Enrich.FromLogContext()
-                        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
+                    services.AddFrameworkPlatform();
+                    services.AddHabitica();
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseSerilog((context, configuration) =>
+                        {
+                            configuration
+                                .MinimumLevel.Debug()
+                                //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                                //.MinimumLevel.Override("System", LogEventLevel.Warning)
+                                //.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                                .Enrich.FromLogContext()
+                                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
+                        });
                 });
         }
 
