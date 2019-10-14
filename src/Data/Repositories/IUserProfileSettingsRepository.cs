d6483cf8cae5813ef45521dceac434875a787665
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-14 13:27:07 +0300

    Update IUserProfileSettingsRepository.cs

--- src/Data/Repositories/IUserProfileSettingsRepository.cs
+++ src/Data/Repositories/IUserProfileSettingsRepository.cs
@@ -10,6 +10,8 @@ namespace TIKSN.Lionize.HabiticaTaskProviderService.Data.Repositories
 {
     public interface IUserProfileSettingsRepository : IMongoRepository<UserProfileSettingsEntity, BigInteger>
     {
+        Task<bool> ExistsHabiticaUserIdAsync(string habiticaUserID, CancellationToken cancellationToken);
+
         Task<IReadOnlyCollection<UserProfileSettingsEntity>> ListAsync(Guid userId, CancellationToken cancellationToken);
 
         Task<IReadOnlyCollection<Guid>> ListUserIdsAsync(CancellationToken cancellationToken);
